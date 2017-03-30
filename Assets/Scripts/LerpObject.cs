9378aed9df8e055993ccf0f9b75fbb8de1f66728
nickeboii <nicke_elmang@hotmail.com>
2017-03-30 16:21:33 +0200

    Lerp teleport

---
+++ Assets/Scripts/LerpObject.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LerpObject : MonoBehaviour 
+{
+
+    public float speed = 0;
+    [Header("Uses this tranform if none is specified for origin.")]
+    public Transform m_Origin;
+    public Transform m_Aim;
+    private Vector3 m_Target;
+    bool targetReached = true;
+    public float m_thresholdDistance;
+
+	// Use this for initialization
+	void Start () 
+    {
+        if (m_Origin == null)
+            m_Origin = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (!targetReached)
+        {
+            print(speed);
+
+            transform.position = Vector3.Lerp(transform.position, m_Target, speed * Time.deltaTime);
+
+            if(Vector3.Distance(m_Origin.position, m_Target) < m_thresholdDistance)
+            {
+                print("yes");
+                targetReached = true;
+            }
+        }
+ 
+	}
+    public void beginLerp()
+    {
+        m_Target = m_Aim.position;
+        targetReached = false;
+    }
+}
--- Assets/Scripts/PlayerTeleport.cs
+++ Assets/Scripts/PlayerTeleport.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class PlayerTeleport : MonoBehaviour {
 
+    public float teleportDistance;
+    private LerpObject lerpObject;
+
 	// Use this for initialization
 	void Start () {
-	
+        lerpObject = GetComponent<LerpObject>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if (Input.GetButtonDown("Fire2"))
+        {
+
+
+            lerpObject.beginLerp();
+            //Quaternion rotation = transform.rotation;
+            //Vector3 direction = rotation.eulerAngles.normalized;
+            //direction *= teleportDistance;
+            //transform.position += direction;
+        }
 	}
 }
