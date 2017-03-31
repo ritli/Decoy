d753fc2f87a1f918bbf0fdab35d0318d42b25d6c
nickeboii <nicke_elmang@hotmail.com>
2017-03-31 10:00:38 +0200

    Started work on raycasting
    ADDED
    Created raycasting script and started writing it.

---
+++ Assets/Scripts/Raycast.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Raycast : MonoBehaviour {
+
+    public Transform raySource;
+    public float maxDistance;
+
+    private RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+    	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        //if (Physics.Raycast()) // Ray ray, out hit, float maxDistance, int layerMask
+        doRaycast();
+    }
+
+    void doRaycast()
+    {
+        Ray ray = new Ray(raySource.position, raySource.forward);
+
+        if (Physics.Raycast(ray, out hit, maxDistance, 1))
+            Debug.DrawRay(ray.origin, ray.direction);
+    }
+}
