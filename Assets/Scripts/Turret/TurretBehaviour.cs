ca763092873aa8f015a0f7443069d2f2a0382639
William Augustsson <willepojken@gmail.com>
2017-04-03 15:01:42 +0200

    Added turret scripts
    [ADDED]
    Added scripts required for turrets

---
+++ Assets/Scripts/Turret/TurretBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretBehaviour : MonoBehaviour {
+
+    int fieldOfView = 10;
+    LookAt m_LookAt;
+    TurretFirepower m_TurretFirepower;
+
+
+    void OnTriggetEnter(Collider other) 
+    {
+        if (other.tag == Tags.player)
+        {
+
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == Tags.player)
+        { 
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
