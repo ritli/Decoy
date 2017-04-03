360a8cac5b3c6d6e547f163dbd34168cea008a5d
nickeboii <nicke_elmang@hotmail.com>
2017-04-03 14:54:50 +0200

    Working on teleport with raycast
    ADDED
    TeleportAdjuster script on teleportTarget
    Player prefab
    
    CHANGES
    Changed player hierarchy
    Changed player teleport

--- Assets/Scripts/PlayerTeleport.cs
+++ Assets/Scripts/PlayerTeleport.cs
@@ -1,47 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/* TODO:
+ * Limit range separately for width and height
+ * Teleport on surface and in the air
+ * 
+ * */
+
 public class PlayerTeleport : MonoBehaviour {
 
+    
     private LerpObject m_lerpObject;
-
-    public Transform target;
-
-    public GameObject spotlight;
-    private Object m_instanceOfSpotlight;
+    private GameObject m_instanceOfteleportTarget;
     private Raycast m_raycaster;
     private Camera m_playerCamera;
-    //private Caps
     private Rigidbody m_rigidbody;
+    private RaycastHit m_rayHit = new RaycastHit();
 
-	// Use this for initialization
-	void Start () {
+    // Parametersssz yes
+    public GameObject teleportTarget;
+
+    void Start() {
         m_lerpObject = GetComponent<LerpObject>();
         m_raycaster = GetComponent<Raycast>();
         m_playerCamera = GetComponentInChildren<Camera>();
         m_rigidbody = GetComponent<Rigidbody>();
+        m_instanceOfteleportTarget = (GameObject)Instantiate(teleportTarget, transform.position, Quaternion.Euler(0, 0, 0));
+        m_instanceOfteleportTarget.transform.SetParent(this.transform);
+        m_instanceOfteleportTarget.SetActive(false);
     }
-	
 	// Update is called once per frame
 	void Update () {
-
+        
+        
         // Create visual indicator of where to teleport
-        if(Input.GetButtonDown("Fire2"))
+        if(Input.GetButton("Teleport"))
         {
-            m_instanceOfSpotlight = Instantiate(spotlight, target.transform.position, Quaternion.Euler(90 ,0, 0));
+
+            if (m_raycaster.doRaycast(out m_rayHit))
+            {
+
+                m_instanceOfteleportTarget.SetActive(true);
+                m_instanceOfteleportTarget.transform.position = m_rayHit.point;
+                
+            }
+            
         }
 
         // Teleport and remove indicator
-	    if (Input.GetButtonUp("Fire2"))
+	    if (Input.GetButtonUp("Teleport"))
         {
-            // Check for collision with raycast.
-            RaycastHit hit = new RaycastHit();
-            if (m_raycaster.doRaycast(out hit))
-            {
-                m_lerpObject.beginLerp(hit.point);
-            }
-
-            Destroy(m_instanceOfSpotlight);
+            m_lerpObject.beginLerp(m_rayHit.point);
+            m_instanceOfteleportTarget.SetActive(false);
+            //Destroy(m_instanceOfteleportTarget);
         }
 	}
 
