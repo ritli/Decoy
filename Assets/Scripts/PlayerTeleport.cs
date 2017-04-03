8bc8d111fc22bc3afc3d63f4f9b84ec8e80b3b13
nickeboii <nicke_elmang@hotmail.com>
2017-04-03 16:54:41 +0200

    Started work on adjusting depending on aim aswell
    CHANGES
    Made teleportTarget active/inactive
    
    ADDED
    RGB-material

--- Assets/Scripts/PlayerTeleport.cs
+++ Assets/Scripts/PlayerTeleport.cs
@@ -11,6 +11,7 @@ public class PlayerTeleport : MonoBehaviour {
 
     
     private LerpObject m_lerpObject;
+    private TeleportationAdjuster m_teleportAdjuster;
     private GameObject m_instanceOfteleportTarget;
     private Raycast m_raycaster;
     private Camera m_playerCamera;
@@ -19,6 +20,7 @@ public class PlayerTeleport : MonoBehaviour {
 
     // Parametersssz yes
     public GameObject teleportTarget;
+    public float teleportDistance;
 
     void Start() {
         m_lerpObject = GetComponent<LerpObject>();
@@ -28,31 +30,40 @@ public class PlayerTeleport : MonoBehaviour {
         m_instanceOfteleportTarget = (GameObject)Instantiate(teleportTarget, transform.position, Quaternion.Euler(0, 0, 0));
         m_instanceOfteleportTarget.transform.SetParent(this.transform);
         m_instanceOfteleportTarget.SetActive(false);
+        m_raycaster.maxDistance = teleportDistance;
+        m_teleportAdjuster = m_instanceOfteleportTarget.GetComponent<TeleportationAdjuster>();
     }
 	// Update is called once per frame
 	void Update () {
         
         
         // Create visual indicator of where to teleport
-        if(Input.GetButton("Teleport"))
+        if(Input.GetButtonDown("Teleport"))
         {
+            m_instanceOfteleportTarget.SetActive(true);
 
+            // Set the target to the point of collision if ray hit, otherwise target is at set distance from player
             if (m_raycaster.doRaycast(out m_rayHit))
             {
-
-                m_instanceOfteleportTarget.SetActive(true);
                 m_instanceOfteleportTarget.transform.position = m_rayHit.point;
-                
             }
-            
+            else
+            {
+                m_instanceOfteleportTarget.transform.position = transform.position + m_playerCamera.transform.forward * teleportDistance;
+            }
         }
 
-        // Teleport and remove indicator
+        // Adjusts the target to where the aim is
+        if (Input.GetButton("Teleport"))
+        {
+            m_teleportAdjuster.adjustToAim(transform.position + m_playerCamera.transform.forward * teleportDistance);
+        }
+
+        // Move towards target when releasing button
 	    if (Input.GetButtonUp("Teleport"))
         {
-            m_lerpObject.beginLerp(m_rayHit.point);
+            m_lerpObject.beginLerp(m_instanceOfteleportTarget.transform.position);
             m_instanceOfteleportTarget.SetActive(false);
-            //Destroy(m_instanceOfteleportTarget);
         }
 	}
 
--- Assets/TeleportationAdjuster.cs
+++ Assets/TeleportationAdjuster.cs
@@ -3,12 +3,23 @@ using System.Collections;
 
 public class TeleportationAdjuster : MonoBehaviour {
 
+    public float threshold;
+
     void OnCollisionStay(Collision terrain)
     {
+        Vector3 averageDirection = new Vector3();
         foreach(ContactPoint contact in terrain.contacts)
         {
             Debug.DrawRay(contact.point, contact.normal * 10, Color.cyan);
+            averageDirection += contact.normal;
         }
-
+        averageDirection /= terrain.contacts.Length;
+        transform.position += averageDirection * threshold;
     }
+
+    public void adjustToAim(Vector3 position)
+    {
+        transform.position = position;
+    }
+
 }
