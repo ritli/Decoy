7b5e3df81e29e92f90707314365733c5b9f2ede1
ritli <agnar_oskarsson@hotmail.com>
2017-04-05 14:54:16 +0200

    Slight changes to raycasting
    CHANGES
    - Indicator, changed origin to camera from player body.
    - Indicator, now raycasts to check for walls if contact point is floor
    - Indicator, now raycasts to check for roof

--- Assets/Scripts/Indicator.cs
+++ Assets/Scripts/Indicator.cs
@@ -5,7 +5,7 @@ public class Indicator : MonoBehaviour {
 
     PlayerController m_player;
     public GameObject m_indi;
-    float m_playerLength;
+    float m_playerLength = 3f;
 
     public float m_length;
 
@@ -39,23 +39,48 @@ public class Indicator : MonoBehaviour {
 
         Vector3 playerLook = forward * m_length;
 
-        print(playerLook);
-
-
-        Ray rayForward = new Ray(transform.position, forward);
+        Ray rayForward = new Ray(Camera.main.transform.position, forward);
         Ray rayDown = new Ray(playerLook, Vector3.down);
 
         RaycastHit hit = new RaycastHit();
 
+        //Debug.DrawRay(Camera.main.transform.position, playerLook, Color.red);
 
         if (Physics.Raycast(rayForward, out hit, m_length))
         {
+            print(Vector3.Angle(hit.normal, Vector3.down));
+
+            if (Vector3.Angle(hit.normal, Vector3.down) == 0)
+            {
+                m_indi.transform.position = hit.point + hit.normal * m_playerLength;
+                return;
+            }
+
+            //If true then surface is wall
             if (Vector3.Angle(hit.normal, Vector3.up) > 45)
             {
                 m_indi.transform.position = hit.point + hit.normal;
             }
+            //If true then normal is a ceiling
+
+            //Else then surface is floor
             else
             {
+                for (int i = 0; i < 5; i++)
+                {
+                    Vector3 centerpos = hit.point + Vector3.up * 0.5f;
+                    Vector3 dir = Quaternion.AngleAxis(i * -45, Vector3.up) * right;
+
+                    if (Physics.Raycast(centerpos, dir, out hit, 1f))
+                    {
+                        
+                        if (Vector3.Angle(hit.normal, Vector3.up) > 45)
+                        {
+                            m_indi.transform.position = hit.point + hit.normal;
+                            return;
+                        }
+                    }
+                }
                 m_indi.transform.position = hit.point + Vector3.up * 0.2f;
                 
             }
@@ -65,7 +90,7 @@ public class Indicator : MonoBehaviour {
 
         else if (Physics.Raycast(rayDown, m_playerLength))
         {
-            //m_indi.transform.position = hit.point + Vector3.up * 0.5f;
+            m_indi.transform.position = hit.point + Vector3.up * 0.5f;
             return;
         }
 
