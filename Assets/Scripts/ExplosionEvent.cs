ed28f4c0f3414ca11751a801519bf4aadb4c3fa6
ritli <agnar_oskarsson@hotmail.com>
2017-04-06 14:51:46 +0200

    Added knullpojke, changed decoy prefab

---
+++ Assets/Scripts/ExplosionEvent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ExplosionEvent : MonoBehaviour {
+
+    ParticleSystem m_particleSystem;
+
+	void Start () {
+
+        m_particleSystem = GetComponent<ParticleSystem>();
+    }
+	
+	void Update () {
+	
+	}
+}
