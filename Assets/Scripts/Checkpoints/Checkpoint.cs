f8d8f4faabf78c8036eae3e9db964ba261f0ca05
William Augustsson <willepojken@gmail.com>
2017-04-07 10:37:25 +0200

    Checkpoints
    [ADDED]
    Checkpoints, basic functionality
    CheckpointEditorManager, to manage and arrange the checkpoints.
    prefab for checkpoints
    [CHANGED]
    Volumetric light, added slider for light multiplier
    William.unity

---
+++ Assets/Scripts/Checkpoints/Checkpoint.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Checkpoint : MonoBehaviour {
+
+    public Transform m_ForcedSpawn;
+    public bool m_UseForcedSpawn = true;
+    private int m_Index = 0;
+    private bool m_Used = false;
+	// Use this for initialization
+	void Start () 
+    {
+        CheckpointEditorManager.UpdateCheckpoints();
+	    m_ForcedSpawn = GetComponentInChildren<Transform>();
+	}
+    public void setIndex(int newIndex)
+    {
+        m_Index = newIndex;
+    }
+    void OnTriggerEnter(Collider collider)
+    {
+        
+        if(collider.transform.gameObject.tag == Tags.player)
+        {
+            if (m_UseForcedSpawn)
+            {
+                PlayerPrefs.SetFloat("spawnX", m_ForcedSpawn.position.x);
+                PlayerPrefs.SetFloat("spawnY", m_ForcedSpawn.position.y);
+                PlayerPrefs.SetFloat("spawnZ", m_ForcedSpawn.position.z);
+            }
+            else
+            {
+                PlayerPrefs.SetFloat("spawnX", collider.transform.position.x);
+                PlayerPrefs.SetFloat("spawnY", collider.transform.position.y);
+                PlayerPrefs.SetFloat("spawnZ", collider.transform.position.z);
+            }
+            PlayerPrefs.Save();
+            m_Used = true;
+        }
+    }
+    public static Vector3 getLatestSave()
+    {
+        Vector3 spawnposition = new Vector3(PlayerPrefs.GetFloat("spawnX"), PlayerPrefs.GetFloat("spawnY"), PlayerPrefs.GetFloat("spawnZ"));
+        return spawnposition;
+    }
+}
---
+++ Assets/Scripts/Checkpoints/CheckpointEditorManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+
+
+[ExecuteInEditMode]
+public class CheckpointEditorManager : MonoBehaviour {
+
+    public static CheckpointEditorManager instance;
+    public string m_NamePrefix = "Checkpoint_";
+	// Use this for initialization
+	void Start () {
+        instance = this;
+	}
+	
+	// Update is called once per frame
+	public static void UpdateCheckpoints () {
+
+	    Checkpoint[] checkpoints = FindObjectsOfType<Checkpoint>();
+        int previousIndex = checkpoints.Length;
+        foreach(Checkpoint chkpt in checkpoints)
+        {
+            string prefix;
+            try 
+            { 
+                prefix = chkpt.gameObject.name.Substring(0, CheckpointEditorManager.instance.m_NamePrefix.Length-1);
+            }
+            catch(IndexOutOfRangeException exception)
+            {
+                Debug.LogError("Incorrect index on Checkpoint: " + exception.Message);
+                return;
+            }
+            if (prefix != CheckpointEditorManager.instance.m_NamePrefix)
+            {
+                chkpt.gameObject.name = CheckpointEditorManager.instance.m_NamePrefix;
+            }
+            int index = 0;
+            try
+            {
+                index = int.Parse(chkpt.gameObject.name.Substring(CheckpointEditorManager.instance.m_NamePrefix.Length));
+            }
+            catch(FormatException exception)
+            {
+                Debug.LogWarning("Incorrect index on Checkpoint: "+exception.Message);
+                chkpt.gameObject.name = chkpt.gameObject.name + --previousIndex;
+                chkpt.setIndex(previousIndex);
+                continue;
+            }
+            if(previousIndex == index+1)
+            {
+                previousIndex = index;
+            }
+            else
+            {
+                chkpt.gameObject.name = chkpt.gameObject.name + --previousIndex;
+                chkpt.setIndex(previousIndex);
+            }
+
+        }
+	}
+}
