1e29c9aa563d01dcc6e458dcba8e68e26fd3d59e
William Augustsson <willepojken@gmail.com>
2017-04-07 16:32:32 +0200

    Checkpoints!
    [CHANGED]
    * checkpoint, saves position of player increases the values of reached
    checkpoint index so that previous checkpoints doesn't save.
    * CheckpointEditorManager, Added option to manually clear playerprefs
    PlayerController, resets position of plaer when a save is present in
    playerprefs.

--- Assets/Scripts/Checkpoints/Checkpoint.cs
+++ Assets/Scripts/Checkpoints/Checkpoint.cs
@@ -11,9 +11,15 @@ public class Checkpoint : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
     {
+        
         CheckpointEditorManager.UpdateCheckpoints();
 	    m_ForcedSpawn = GetComponentInChildren<Transform>();
+        print("Checkpoint index: " + m_Index);
 	}
+    void OnDisable()
+    {
+        CheckpointEditorManager.UpdateCheckpoints();
+    }
     public void setIndex(int newIndex)
     {
         m_Index = newIndex;
@@ -21,27 +27,66 @@ public class Checkpoint : MonoBehaviour {
     void OnTriggerEnter(Collider collider)
     {
         
-        if(collider.transform.gameObject.tag == Tags.player)
+        if(collider.transform.gameObject.tag == Tags.player && getReachedCheckpoint() < m_Index)
         {
+            PlayerPrefs.SetInt("CheckpointIndex", m_Index);
+            print("Index set to " + m_Index);
+
             if (m_UseForcedSpawn)
             {
-                PlayerPrefs.SetFloat("spawnX", m_ForcedSpawn.position.x);
-                PlayerPrefs.SetFloat("spawnY", m_ForcedSpawn.position.y);
-                PlayerPrefs.SetFloat("spawnZ", m_ForcedSpawn.position.z);
+                
+
+                PlayerPrefs.SetFloat("PositionX", m_ForcedSpawn.position.x);
+                PlayerPrefs.SetFloat("PositionY", m_ForcedSpawn.position.y);
+                PlayerPrefs.SetFloat("PositionZ", m_ForcedSpawn.position.z);
+
+                PlayerPrefs.SetFloat("RotationX", m_ForcedSpawn.rotation.x);
+                PlayerPrefs.SetFloat("RotationY", m_ForcedSpawn.rotation.y);
+                PlayerPrefs.SetFloat("RotationZ", m_ForcedSpawn.rotation.z);
+                PlayerPrefs.SetFloat("RotationW", m_ForcedSpawn.rotation.w);
+
             }
             else
             {
-                PlayerPrefs.SetFloat("spawnX", collider.transform.position.x);
-                PlayerPrefs.SetFloat("spawnY", collider.transform.position.y);
-                PlayerPrefs.SetFloat("spawnZ", collider.transform.position.z);
+                PlayerPrefs.SetFloat("PositionX", collider.transform.position.x);
+                PlayerPrefs.SetFloat("PositionY", collider.transform.position.y);
+                PlayerPrefs.SetFloat("PositionZ", collider.transform.position.z);
+
+                PlayerPrefs.SetFloat("RotationX", collider.transform.rotation.x);
+                PlayerPrefs.SetFloat("RotationY", collider.transform.rotation.y);
+                PlayerPrefs.SetFloat("RotationZ", collider.transform.rotation.z);
+                PlayerPrefs.SetFloat("RotationW", collider.transform.rotation.w);
             }
             PlayerPrefs.Save();
             m_Used = true;
         }
     }
-    public static Vector3 getLatestSave()
+    public static bool isPreviouslySaved()
     {
-        Vector3 spawnposition = new Vector3(PlayerPrefs.GetFloat("spawnX"), PlayerPrefs.GetFloat("spawnY"), PlayerPrefs.GetFloat("spawnZ"));
-        return spawnposition;
+        if(PlayerPrefs.HasKey("PositionX"))
+        {
+            return true;
+        }
+        return false;
+    }
+    public static Vector3 getSavedPlayerPosition()
+    {
+        print("Position acquired");
+        Vector3 spawnPosition = new Vector3(PlayerPrefs.GetFloat("PositionX"), PlayerPrefs.GetFloat("PositionY"), PlayerPrefs.GetFloat("PositionZ"));
+        
+        return spawnPosition;
+    }
+    public static Quaternion getSavedPlayerRotation()
+    {
+        Quaternion spawnRotation = new Quaternion(PlayerPrefs.GetFloat("RotationX"), PlayerPrefs.GetFloat("RotationY"), PlayerPrefs.GetFloat("RotationZ"), PlayerPrefs.GetFloat("RotationW"));
+        return spawnRotation;   
+    }
+    public int getReachedCheckpoint()
+    {
+        if (PlayerPrefs.HasKey("CheckpointIndex"))
+        {
+            return PlayerPrefs.GetInt("CheckpointIndex");
+        }
+        return -1;
     }
 }
--- Assets/Scripts/Checkpoints/CheckpointEditorManager.cs
+++ Assets/Scripts/Checkpoints/CheckpointEditorManager.cs
@@ -7,56 +7,72 @@ using System;
 [ExecuteInEditMode]
 public class CheckpointEditorManager : MonoBehaviour {
 
+    public bool m_ClearPlayerPrefs = false;
     public static CheckpointEditorManager instance;
     public string m_NamePrefix = "Checkpoint_";
 	// Use this for initialization
-	void Start () {
+	void OnEnable()
+    {
         instance = this;
-	}
+    }
 	
-	// Update is called once per frame
 	public static void UpdateCheckpoints () {
 
 	    Checkpoint[] checkpoints = FindObjectsOfType<Checkpoint>();
-        int previousIndex = checkpoints.Length;
+        int index = checkpoints.Length-1;
+        //set name and index on the checkpoint
         foreach(Checkpoint chkpt in checkpoints)
         {
-            string prefix;
-            try 
-            { 
-                prefix = chkpt.gameObject.name.Substring(0, CheckpointEditorManager.instance.m_NamePrefix.Length-1);
-            }
-            catch(IndexOutOfRangeException exception)
-            {
-                Debug.LogError("Incorrect index on Checkpoint: " + exception.Message);
-                return;
-            }
-            if (prefix != CheckpointEditorManager.instance.m_NamePrefix)
-            {
-                chkpt.gameObject.name = CheckpointEditorManager.instance.m_NamePrefix;
-            }
-            int index = 0;
-            try
-            {
-                index = int.Parse(chkpt.gameObject.name.Substring(CheckpointEditorManager.instance.m_NamePrefix.Length));
-            }
-            catch(FormatException exception)
-            {
-                Debug.LogWarning("Incorrect index on Checkpoint: "+exception.Message);
-                chkpt.gameObject.name = chkpt.gameObject.name + --previousIndex;
-                chkpt.setIndex(previousIndex);
-                continue;
-            }
-            if(previousIndex == index+1)
-            {
-                previousIndex = index;
-            }
-            else
-            {
-                chkpt.gameObject.name = chkpt.gameObject.name + --previousIndex;
-                chkpt.setIndex(previousIndex);
-            }
 
+            chkpt.gameObject.name = instance.m_NamePrefix;
+            chkpt.gameObject.name += index;
+            chkpt.setIndex(index--);
+
+            #region Oldmethod
+            //string prefix;
+            //try 
+            //{ 
+            //    prefix = chkpt.gameObject.name.Substring(0, CheckpointEditorManager.instance.m_NamePrefix.Length-1);
+            //}
+            //catch(IndexOutOfRangeException exception)
+            //{
+            //    Debug.LogError("Incorrect prefixon Checkpoint: " + exception.Message);
+            //    return;
+            //}
+            //if (prefix != CheckpointEditorManager.instance.m_NamePrefix)
+            //{
+            //    chkpt.gameObject.name = CheckpointEditorManager.instance.m_NamePrefix;
+            //}
+            //try
+            //{
+            //    index = int.Parse(chkpt.gameObject.name.Substring(CheckpointEditorManager.instance.m_NamePrefix.Length));
+            //}
+            //catch(FormatException exception)
+            //{
+            //    Debug.LogWarning("Incorrect index on Checkpoint: "+exception.Message);
+            //    chkpt.gameObject.name = chkpt.gameObject.name + --previousIndex;
+            //    chkpt.setIndex(previousIndex);
+            //    continue;
+            //}
+            //if(previousIndex == index+1)
+            //{
+            //    previousIndex = index;
+            //}
+            //else
+            //{
+            //    chkpt.gameObject.name = chkpt.gameObject.name + --previousIndex;
+            //    chkpt.setIndex(previousIndex);
+            //}
+            #endregion
         }
 	}
+    void Update()
+    {
+        if(m_ClearPlayerPrefs)
+        {
+            m_ClearPlayerPrefs = false;
+
+            PlayerPrefs.DeleteAll();
+        }
+    }
 }
