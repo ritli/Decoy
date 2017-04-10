fd4c91ba2922ee507e3cded9d2ecbfd159c1d7a9
William Augustsson <willepojken@gmail.com>
2017-04-10 15:14:53 +0200

    Initial publish
    [ADDED]
    Initial files for MainMenu
    [CHANGES]
    GameManager.cs, added functions to apply values provided by options
    submenu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public void changeScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+    public void setGameObjectActive(GameObject target)
+    {
+        target.SetActive(true);   
+    }
+    public void setGameObjectInactive(GameObject target)
+    {
+        target.SetActive(false);
+    }
+    public void continueGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void LoadFromCheckpoint(int index)
+    {
+         
+    }
+}
---
+++ Assets/Scripts/OptionsApplier.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class OptionsApplier : MonoBehaviour
+{
+
+    private Slider[] sliders;
+
+	// Use this for initialization
+	void Start ()
+    {
+        sliders = GetComponentsInChildren<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GameManager.applySettings(sliders[0].value, sliders[0].value, sliders[0].value);
+	}
+}
