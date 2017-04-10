6ed4e7b5c5ab233353d0dcd147904a2e2433775d
nickeboii <niclaselmang@Niclass-MacBook-Pro-3.local>
2017-04-10 14:46:48 +0200

    Fixed ledgeGrab edge case
    CHANGES
    ledgeDetection now raycasts a second time to figure out if the first
    ray was inside a collider

--- Assets/Scripts/LedgeDetection.cs
+++ Assets/Scripts/LedgeDetection.cs
@@ -5,11 +5,13 @@ public class LedgeDetection : MonoBehaviour {
 
     private Vector3 m_wallNormal;
     private Ray m_rayDown;
+	private Ray m_rayDown2;
 	private Vector3 m_newPosition = new Vector3(0, 0, 0);
 
 	[Header("Distance from ledge:")]
     public float ledgeThreshold;
-	public float positionOffset;
+
+	private float positionOffset = 0.5f;
 
 
     // Use this for initialization
@@ -22,7 +24,7 @@ public class LedgeDetection : MonoBehaviour {
 
 		// Creates new direction and position based on wall hit
 		Vector3 direction = new Vector3(wallHit.normal.x, wallHit.normal.y, wallHit.normal.z) * -1;
-		Debug.DrawRay(wallHit.point, direction * 4, Color.blue);
+//		Debug.DrawRay(wallHit.point, direction * 4, Color.blue);
 
 		// Creates a right vector based on wallNormal and up
 		Vector3 hitRight = Vector3.Cross(Vector3.up, wallHit.normal);
@@ -30,10 +32,8 @@ public class LedgeDetection : MonoBehaviour {
 		// Rotates around the right vector to face downward
         direction = Quaternion.AngleAxis(-90f, hitRight) * direction.normalized;
 		
-		Debug.DrawRay(wallHit.point, hitRight * 4, Color.magenta);
-		Debug.DrawRay(wallHit.point, direction * 4, Color.grey);
-		
-		//print("wallHit Y: " + wallHit.normal.y);
+//		Debug.DrawRay(wallHit.point, hitRight * 4, Color.magenta);
+//		Debug.DrawRay(wallHit.point, direction * 4, Color.grey);
 	
 		// Creates a point above and a little bit inside the wall to look for floor
 		Vector3 newPosition = wallHit.point + Vector3.up * ledgeThreshold - wallHit.normal * positionOffset;
@@ -42,13 +42,22 @@ public class LedgeDetection : MonoBehaviour {
 		m_rayDown.origin = newPosition;
         m_rayDown.direction = direction;
 
+		// Another ray used to determine if other ray is inside terrain
+		m_rayDown2.origin = newPosition + Vector3.up * 2;
+		m_rayDown2.direction = direction;
+
+
         RaycastHit hit = new RaycastHit();
 
 		// Ray based on given wallNormal
-		Debug.DrawRay(wallHit.point, wallHit.normal * 4, Color.green);
+//		Debug.DrawRay(wallHit.point, wallHit.normal * 4, Color.green);
 
 		// Ray moved up and looking down based on wallNormal
 		Debug.DrawRay(m_rayDown.origin, m_rayDown.direction * 4, Color.yellow);
+
+		Debug.DrawRay(m_rayDown2.origin, m_rayDown2.direction * 4, Color.cyan);
+
+		Vector3 rayDownNormal;
 
 		// If ray hit the floor
 		if (Physics.Raycast(m_rayDown, out hit))
@@ -57,14 +66,30 @@ public class LedgeDetection : MonoBehaviour {
 			{
 
 				// Ray from the floor above ledge
-				Debug.DrawRay(hit.point, hit.normal * 10, Color.white);
+				Debug.DrawRay(hit.point, hit.normal * 5, Color.white);
 
 				// If the point is above where the aim is
 				if (hit.point.y >= wallHit.point.y)
 				{
+					// Store the normal for later use
+					rayDownNormal = hit.normal;
 
 					// Set the new position
 					m_newPosition = hit.point;
+
+					// Raycast again to check if the first was inside an object
+					if (Physics.Raycast(m_rayDown2, out hit)) 
+					{
+						
+//						Debug.DrawRay(hit.point, hit.normal * 5, Color.red);
+
+						// If the angle between the two rays is different, then the first ray was inside an object
+						if (Vector3.Angle (rayDownNormal, hit.normal) > 0) 
+						{
+							// Update the position to the higher point
+							m_newPosition = hit.point;
+						}
+					}
 
 					return true;
 				}
--- Assets/Scripts/TeleportationAdjuster.cs
+++ Assets/Scripts/TeleportationAdjuster.cs
@@ -7,17 +7,52 @@ public class TeleportationAdjuster : MonoBehaviour {
 
     private Vector3 m_offset;
 
-    void OnCollisionStay(Collision terrain)
-    {
-        Vector3 averageDirection = new Vector3();
-        foreach(ContactPoint contact in terrain.contacts)
-        {
-            Debug.DrawRay(contact.point, contact.normal * 10, Color.cyan);
-            averageDirection += contact.normal;
-        }
-        averageDirection /= terrain.contacts.Length;
-        m_offset = averageDirection * threshold;
-    }
+	private Vector3 m_direction = new Vector3(0, 0, 0);
+	private float m_distance = 0;
+	private Rigidbody rb;
+
+	void Start() 
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void OnCollisionStay(Collision terrain) 
+	{
+		Collider thisCollider = GetComponent<Collider> ();
+		Collider otherCollider = terrain.collider;
+
+		print (terrain.transform.name);
+
+		if (Physics.ComputePenetration (
+			thisCollider, transform.position, transform.rotation,
+			otherCollider, terrain.transform.position, terrain.transform.rotation, 
+				out m_direction, out m_distance)) 
+		{
+			Debug.DrawRay (terrain.contacts [0].point, m_direction * m_distance, Color.cyan, 10);
+			transform.position += m_direction * m_distance;
+		}
+	}
+
+	public void checkTeleLocation(float distance)
+	{
+		RaycastHit hit;
+		if (rb.SweepTest (transform.forward, out hit, distance)) 
+		{
+			
+		}
+	}
+
+//    void OnCollisionStay(Collision terrain)
+//    {
+//        Vector3 averageDirection = new Vector3();
+//        foreach(ContactPoint contact in terrain.contacts)
+//        {
+//            Debug.DrawRay(contact.point, contact.normal * 10, Color.cyan);
+//            averageDirection += contact.normal;
+//        }
+//        averageDirection /= terrain.contacts.Length;
+//        m_offset = averageDirection * threshold;
+//    }
 
     //void OnCollisionExit()
     //{
