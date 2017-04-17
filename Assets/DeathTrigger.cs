117caff0fe7a40bd5adb2dd1f9745ffd8134d20c
ritli <agnar_oskarsson@hotmail.com>
2017-04-17 10:20:22 +0200

    Added simple death trigger, chanees to gameplay test scene

---
+++ Assets/DeathTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTrigger : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.GetComponent<PlayerController>())
+        {
+            IKillable killable = GameManager.GetPlayer();
+            killable.Kill();
+        }
+    }
+}
