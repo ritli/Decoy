5c35d43211767b1a637ffc85e82ba1cee8f0ba26
William Augustsson <willepojken@gmail.com>
2017-04-19 16:13:50 +0200

    Doors can be saved.
    [CHANGES]
    * DoorBehaviour, doors can be saved.
    * GameManager, doesn't call for reset (saveload) as Playercontroller
    already does this.

--- Assets/Scripts/DoorBehaviour.cs
+++ Assets/Scripts/DoorBehaviour.cs
@@ -13,21 +13,40 @@ public class DoorBehaviour : ActivationObject{
     public DoorStates m_DisableAfterState;
 
     private Animator m_Animator;
+    private DoorStates m_initialState;
+
+    public Color m_LockedColor;
+    public Color m_UnlockedColor;
+    private Light[] lockLights;
 
     // Use this for initialization
     void Start () {
+
+        lockLights = GetComponentsInChildren<Light>();
+
         m_Animator = GetComponent<Animator>();
-        if(m_Animator == null)
+
+        if (m_Animator == null)
         {
             Debug.LogError("No Animator found.");
         }
-        //Sets the door open or closed
+
         m_Animator.SetBool("IsOpen", m_IsOpen);
 
-        if(m_IsOpen)
-            m_Animator.Play("OpenIdle");
+        //Set door to finished state if checkpoint was reached.
+        if (m_initialState != DoorStates.None)
+        {
+            Debug.Log("Setting saved state..");
+            m_Animator.Play(m_initialState.ToString());
+        }
         else
-            m_Animator.Play("CloseIdle");
+        {
+            //Set door to configured state because either no save or not enough progress on map.
+            if (m_IsOpen)
+                m_Animator.Play("OpenIdle");
+            else
+                m_Animator.Play("CloseIdle");
+        }
 
         //Warn the designers if they use an obsolete combination of variables.
         if (m_IsOpen && m_DisableAfterState == DoorStates.Open)
@@ -38,6 +57,18 @@ public class DoorBehaviour : ActivationObject{
         //if both m_IsOpen and m_DisableAfterState 
         //is true/open or false/close then the door wont react to player interction
 
+
+        foreach (Light light in lockLights)
+        {
+            if (m_DisableDoor)
+            {
+                light.color = m_LockedColor;
+            }
+            else
+            {
+                light.color = m_UnlockedColor;
+            }
+        }
     }
     // Subscribe and desubscribe
     void OnEnable()
@@ -50,6 +81,7 @@ public class DoorBehaviour : ActivationObject{
         // Desubscribe OnActivationReset to the event
         GameManager.OnActivationReset -= checkActivationEvent;
     }
+    
     public override void activate()
     {
         if (!m_DisableDoor)
@@ -83,6 +115,7 @@ public class DoorBehaviour : ActivationObject{
 
     protected override void checkActivationEvent(int index)
     {
+        print("Save check performed.");
         //this checks if we should be open or close
         if(m_SaveDoor && checkIndex <= index)
         {
@@ -90,15 +123,17 @@ public class DoorBehaviour : ActivationObject{
             if(!m_IsOpen)
             {
                 print("Let the door be open.");
-                m_Animator.Play(DoorStates.OpenIdle.ToString());
+                m_initialState = DoorStates.OpenIdle;
+
             }
             //if the door was set to be open from the begninning, then the finished state is to be closed
             else
             {
                 print("Let the door be closed");
-                m_Animator.Play(DoorStates.CloseIdle.ToString());
+                m_initialState = DoorStates.CloseIdle;
             }
             m_IsOpen = !m_IsOpen;
         }
+        m_initialState = DoorStates.None;
     }
 }
