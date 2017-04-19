73f57f32f881c93e91138557d6d8c718a0655d52
William Augustsson <willepojken@gmail.com>
2017-04-19 09:24:19 +0200

    Scripts and prefab
    [ADDED]
    * Door, prefab, animation, scripts to handle animation and triggering of
    door
    [REMOVED]
    * SimpleReset, obsolete
    * Spotlight, obsolete

---
+++ Assets/Scripts/DoorBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehaviour : ActivationObject{
+
+    private Animator m_Animator;
+
+	// Use this for initialization
+	void Start () {
+        m_Animator = GetComponent<Animator>();
+        if(m_Animator == null)
+        {
+            Debug.LogError("No Animator found.");
+        }
+        m_Animator.Play("Open");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public override void activate()
+    {
+        m_Animator.Play("Open");
+        throw new NotImplementedException();
+    }
+    public override void deactivate()
+    {
+        m_Animator.Play("Close");
+        throw new NotImplementedException();
+    }
+    public override bool isActivated()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void checkActivationEvent(int index)
+    {
+        throw new NotImplementedException();
+    }
+}
