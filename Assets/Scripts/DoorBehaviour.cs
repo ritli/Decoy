71e553b9616e3017093848fa1bbcca3a9a286de1
William Augustsson <willepojken@gmail.com>
2017-04-19 14:04:23 +0200

    Kind of working door
    [ADDED]
    *Door prefab
    *Door animations
    [CHANGES]
    * William.unity
    *DoorBehaviour, can start open/closed, can be disabled after an call for
    closing/opening, can be disabled.

--- Assets/Scripts/DoorBehaviour.cs
+++ Assets/Scripts/DoorBehaviour.cs
@@ -5,45 +5,100 @@ using UnityEngine;
 
 public class DoorBehaviour : ActivationObject{
 
+    public enum DoorStates { Open, OpenIdle, Close,CloseIdle};
+
+    public bool m_SaveDoor = false;
+    public bool m_DisableDoor = false;
+    public bool m_IsOpen = false;
+    public DoorStates m_DisableAfterState;
+
     private Animator m_Animator;
-    private bool m_IsActivated = false;
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
         m_Animator = GetComponent<Animator>();
         if(m_Animator == null)
         {
             Debug.LogError("No Animator found.");
         }
-        //m_Animator.Play("Open");
+        //Sets the door open or closed
+        m_Animator.SetBool("IsOpen", m_IsOpen);
+
+        if(m_IsOpen)
+            m_Animator.Play("OpenIdle");
+        else
+            m_Animator.Play("CloseIdle");
+
+        //Warn the designers if they use an obsolete combination of variables.
+        if (m_IsOpen && m_DisableAfterState == DoorStates.Open)
+            Debug.LogWarning("Obsolete door: m_IsOpen is true and m_DisableAfterState is set to Open");
+        else if (!m_IsOpen && m_DisableAfterState == DoorStates.Close)
+            Debug.LogWarning("Obsolete door: m_IsOpen is false and m_DisableAfterState is set to Close");
+
+        //if both m_IsOpen and m_DisableAfterState 
+        //is true/open or false/close then the door wont react to player interction
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Subscribe and desubscribe
+    void OnEnable()
+    {
+        // Subscribe OnActivationReset to the event
+        GameManager.OnActivationReset += checkActivationEvent;
+    }
+    void OnDisable()
+    {
+        // Desubscribe OnActivationReset to the event
+        GameManager.OnActivationReset -= checkActivationEvent;
+    }
     public override void activate()
     {
-        m_IsActivated = true;
-        if(!m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Close"))
-            m_Animator.Play("Open");
-        m_Animator.SetBool("IsOpen", true);
-
+        if (!m_DisableDoor)
+        {
+            m_IsOpen = true;
+            m_Animator.SetBool("IsOpen", m_IsOpen);
+            
+            if (m_DisableAfterState == DoorStates.Open)
+            {
+                m_DisableDoor = true;
+            }
+        }
     }
     public override void deactivate()
     {
-        m_IsActivated = false;
-        if (!m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Open"))
-            m_Animator.Play("Close");
-        m_Animator.SetBool("IsOpen", false);
+        if (!m_DisableDoor)
+        {
+            m_IsOpen = false;
+            m_Animator.SetBool("IsOpen", m_IsOpen);
+
+            if (m_DisableAfterState == DoorStates.Close)
+            {
+                m_DisableDoor = true;
+            }
+        }
     }
     public override bool isActivated()
     {
-        return m_IsActivated;   
+        return m_IsOpen;   
     }
 
     protected override void checkActivationEvent(int index)
     {
-        throw new NotImplementedException();
+        //this checks if we should be open or close
+        if(m_SaveDoor && checkIndex <= index)
+        {
+            //if the door was set to be closed from the beginning, then the finished state is to be open
+            if(!m_IsOpen)
+            {
+                print("Let the door be open.");
+                m_Animator.Play(DoorStates.OpenIdle.ToString());
+            }
+            //if the door was set to be open from the begninning, then the finished state is to be closed
+            else
+            {
+                print("Let the door be closed");
+                m_Animator.Play(DoorStates.CloseIdle.ToString());
+            }
+            m_IsOpen = !m_IsOpen;
+        }
     }
 }
