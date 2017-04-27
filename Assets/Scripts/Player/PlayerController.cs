60a8cc478c07f52aafa4b8e09ec433b070a526d9
Eric <eric-noren@hotmail.com>
2017-04-27 12:01:11 +0200

    Worked on aircontrols

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -59,6 +59,8 @@ public class PlayerController : MonoBehaviour, IKillable
     [Range(0, 50)]
     public float m_stationaryJumpAirControl;
     private Vector3 signedJumpVector;
+    private float m_airDecreaseY;
+    private float m_airDecreaseX;
 
     //Gravity vars
     [Header("Gravity Variables")]
@@ -561,22 +563,42 @@ public class PlayerController : MonoBehaviour, IKillable
         //Always move along the camera forward as it is the direction that it being aimed at
         Vector3 desiredMove = transform.forward * Input.y + transform.right * Input.x;
 
+
         if (m_Jumping)
         {
             desiredMove = m_jumpVector;
 
-            Vector3 newVector = m_jumpVector + transform.forward * GetInput().y * m_JumpAirControl 
-                + transform.right * GetInput().x * m_JumpAirControl;
+            //Vector3 newVector = m_jumpVector + transform.forward * GetInput().y * m_JumpAirControl 
+            //    + transform.right * GetInput().x * m_JumpAirControl;
 
+            // Move L/R
             m_jumpVector += transform.right * GetInput().x * m_JumpAirControl;
 
-            if (signedJumpVector.x == Mathf.Sign(newVector.x)
-                && signedJumpVector.z == Mathf.Sign(newVector.z))
+            m_airDecreaseY += GetInput().y * m_JumpAirControl;
+            m_airDecreaseX += GetInput().x * m_JumpAirControl;
+
+            // >= 0: Transform moving forward, otherwise: Backwards. Update speed change accordingly.
+            if (Vector3.Dot(transform.forward, desiredMove) >= 0)
             {
-                m_jumpVector += transform.forward * GetInput().y * m_JumpAirControl;
+                speed += m_airDecreaseY;
+            }
+            else
+            {
+                speed -= m_airDecreaseY;
             }
 
-            print("JumpVector: " + m_jumpVector);
+            if (Vector3.Dot(transform.right, desiredMove) >= 0)
+            {
+                speed += m_airDecreaseX;
+            }
+            else
+            {
+                speed -= m_airDecreaseX;
+            }
+
+            // Make sure the player cannot accelerate by moving in the air.
+            speed = Mathf.Clamp(speed, 0, m_WalkSpeed);
+            print(speed);
         }
 
         //Get a normal for the surface that is being touched to move along it
@@ -584,6 +606,8 @@ public class PlayerController : MonoBehaviour, IKillable
         Physics.SphereCast(transform.position, m_CharacterController.radius, Vector3.down, out hitInfo, m_CharacterController.height / 2f, Physics.AllLayers, QueryTriggerInteraction.Ignore);
 
         desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
+
+        //print("Desired movement: " + desiredMove);
 
         // If stationary when jumping, use stationary force instead of ordinary speed. Otherwise apply speed as normal.
         if (speed == 0 && m_Jumping)
@@ -610,6 +634,8 @@ public class PlayerController : MonoBehaviour, IKillable
                 m_Jump = false;
                 m_Jumping = true;
                 signedJumpVector = new Vector3(Mathf.Sign(m_MoveDir.x), 0, Mathf.Sign(m_MoveDir.z));
+                m_airDecreaseY = 0.0f;
+                m_airDecreaseX = 0.0f;
             }
 
         }
