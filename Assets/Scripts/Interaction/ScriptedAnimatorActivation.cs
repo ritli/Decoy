10ea4b3bd6f14bccfda9ca97658c8b2cd9df1087
Eric <eric-noren@hotmail.com>
2017-05-10 17:00:20 +0200

    Started work on scripted event
    ADDED
    * ScriptedAnimatorActivation.cs class to animate and move the scientist
    in sect 1a
    
    CHANGES
    * Events in ActivationSequence can now be named for convenience

---
+++ Assets/Scripts/Interaction/ScriptedAnimatorActivation.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct Event
+{
+    [Tooltip("Name of event")]
+    public string eventName;
+    [Tooltip("The position to move to")]
+    public Transform movePosition;
+    [Tooltip("Determine how fast the movement is performed")]
+    public float moveSpeed;
+    [Tooltip("Degrees to turn")]
+    public float rotation;
+    [Tooltip("Name the animation state to be played")]
+    public string stateToPlay;
+    [Tooltip("Determine time until event is executed")]
+    public float timeUntilEvent;
+}
+
+[RequireComponent(typeof(Timer))]
+public class ScriptedAnimatorActivation : ActivationObject {
+
+    // Privates
+    private Animator m_animator;
+    private List<string> animStates;
+    private bool startedScriptedEvent = false;
+
+    // Publics
+    public Event[] events;
+
+    public override void activate()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void deactivate()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool isActivated()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void checkActivationEvent(int index)
+    {
+        throw new NotImplementedException();
+    }
+
+	// Use this for initialization
+	void Awake ()
+    {
+        m_animator = GetComponent<Animator>();
+
+        //m_animator.
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // JUST A TEST
+        m_animator.Play("Scientist Appear Turn");
+	}
+}
