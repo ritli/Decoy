016cdf4dd288283d9a9d97da3ede17c84980cf91
Eric <eric-noren@hotmail.com>
2017-05-11 15:55:28 +0200

    Finished first version of scientist animation

--- Assets/Scripts/Interaction/ScriptedAnimatorActivation.cs
+++ Assets/Scripts/Interaction/ScriptedAnimatorActivation.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ScientistState { Aiming, AppearTurn, Fibble, Idle, RemoveAndLeave, Walk };
+public enum ScientistState { Aiming, AppearTurn, Fibble, Idle, RemoveAndLeave, Walk, None };
 
 [Serializable]
 public struct Event
@@ -16,10 +16,17 @@ public struct Event
     public float moveSpeed;
     [Tooltip("Degrees to turn")]
     public float rotation;
+    [Tooltip("Determine how fast the rotation is performed.")]
+    public float rotationSpeed;
     [Tooltip("Choose the animation state to be played")]
     public  ScientistState stateToPlay;
     [Tooltip("Determine time until event is executed")]
     public float timeUntilEvent;
+    [Header("Conditions to await")]
+    [Tooltip("Determine if this event should wait for the movement of the previous one")]
+    public bool awaitPrevMovement;
+    [Tooltip("Determine if this event should wait for this animation state before running. Leave empty if not used.")]
+    public string awaitState;
 }
 
 [RequireComponent(typeof(Timer))]
@@ -32,10 +39,15 @@ public class ScriptedAnimatorActivation : ActivationObject {
     private Timer m_timer;
     private int currentIndex = 0;
 
-    // Movement and Rotation vars
+    // Movement vars
     private bool m_isMoving = false;
     private Vector3 m_targetPosition = new Vector3(0, 0, 0);
+    private float currentMovementSpeed;
+
+    // Rotation vars
     private bool m_isRotating = false;
+    private Quaternion m_targetRotation;
+    private float currentRotationSpeed;
 
     // Publics
     public ScientistState defaultState;
@@ -87,46 +99,74 @@ public class ScriptedAnimatorActivation : ActivationObject {
     void Update ()
     {
         // Play the next event when time is up
-        if (m_timer.isTimeUp())
+        if (m_timer.isTimeUp() && startedScriptedEvent)
         {
             Event cEvent = events[currentIndex];
-            playState(cEvent.stateToPlay);
-
-            if (cEvent.movePosition != null)
+            
+            if (cEvent.awaitPrevMovement && !m_isMoving
+                || !cEvent.awaitPrevMovement)
             {
-                m_targetPosition = cEvent.movePosition.position;
-                m_isMoving = true;
+                if (m_animator.GetCurrentAnimatorStateInfo(0).IsName(cEvent.awaitState)
+                || cEvent.awaitState == "")
+                {
+                    playState(cEvent.stateToPlay);
+
+                    if (cEvent.movePosition != null)
+                    {
+                        m_targetPosition = cEvent.movePosition.position;
+                        currentMovementSpeed = cEvent.moveSpeed;
+                        m_isMoving = true;
+                    }
+
+                    if (cEvent.rotation != 0)
+                    {
+                        m_targetRotation = Quaternion.AngleAxis(cEvent.rotation, new Vector3(0, 1, 0)) * transform.rotation;
+                        currentRotationSpeed = cEvent.rotationSpeed;
+                        m_isRotating = true;
+                    }
+
+                    if (currentIndex < events.Length - 1)
+                        currentIndex++;
+                    else
+                        startedScriptedEvent = false;
+
+                    // Start timer for next event
+                    m_timer.setTimeout(events[currentIndex].timeUntilEvent);
+                    m_timer.resetTimer();
+                }
+
             }
-
-            if (currentIndex < events.Length - 1)
-                currentIndex++;
-            else
-                Destroy(GetComponent<ScriptedAnimatorActivation>());
-
-            // Start timer for next event
-            m_timer.setTimeout(events[currentIndex].timeUntilEvent);
-            m_timer.resetTimer();
         }
 
         // Start moving towards position if one is assigned
         if (m_isMoving)
         {
-            transform.position = Vector3.MoveTowards(transform.position, m_targetPosition, events[currentIndex].moveSpeed * Time.deltaTime);
-            print("MOVING LOL");
+            transform.position = Vector3.MoveTowards(transform.position, m_targetPosition, currentMovementSpeed * Time.deltaTime);
             if (Vector3.Distance(transform.position, m_targetPosition) == 0)
             {
                 m_isMoving = false;
-                print("Stop Moving!");
-            }
 
+                if (currentState == ScientistState.Walk)
+                    playState(ScientistState.Idle);
+            }
+        }
+
+        if (m_isRotating)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, m_targetRotation, currentRotationSpeed * Time.deltaTime);
+
+            if (transform.rotation == m_targetRotation)
+                m_isRotating = false;
         }
 	}
 
     // Play the given state
     void playState(ScientistState playState)
     {
+        currentState = playState;
+
         // Disable the animators walking loop if state to be played is not "Walk"
-        if (playState != ScientistState.Walk)
+        if (playState != ScientistState.Walk && playState != ScientistState.None)
             m_animator.SetBool("IsWalking", false);
 
         switch (playState)
@@ -143,13 +183,16 @@ public class ScriptedAnimatorActivation : ActivationObject {
                 break;
 
             case ScientistState.Idle:
-                m_animator.Play("Scientist Idle");
+                //m_animator.Play("Scientist Idle");
                 break;
             case ScientistState.RemoveAndLeave:
                 m_animator.Play("Scientist Remove And Leave");
                 break;
             case ScientistState.Walk:
                 m_animator.SetBool("IsWalking", true);
+                m_animator.Play("Scientist Walk");
+                break;
+            default:
                 break;
         }
     }
