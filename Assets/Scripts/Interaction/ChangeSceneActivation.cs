87d20b6ac146359773148bd2ceffd97dc8236dfc
William Augustsson <willepojken@gmail.com>
2017-05-15 20:48:22 +0200

    Ending, credits, small fixes
    [ADDED]
    * CreditScene
    * C_Gamelogo
    * ImageFaderActivation, An activation to change state of Imagefader
    * ChangeSceneActivation, An activation to load a single scene +
    initialscene
    [CHANGES]
    * EndStartTrigger, changed positon, and added a sequence.
    * MainMenu, edited menu to be able to exit, changed header to a nicer
    one.
    * MainMenuCanvas, see MainMenu.
    * CreditScene, rolls credits, needs script to take input during roll.
    * WilliamScene, testing out EndStarter.
    * ActivationSequence, Added a Trigger type.
    * EndStarter, Added several lines to further improve the "freeze" of the
    end.
    * SceneLoader, added CreditScene to Scenes enum, Added methof to unload
    all scenes, except InitialScene.
    * PlayerController, Added method to stop bob, used by EndStarter.
    * PauseManager, changed timescale = 0 to timescale = 0.00000000001f; to
    remove the "white corners" bug caused by a postprocesseffect.
    * EditorBuildSettings, added creditScene.

---
+++ Assets/Scripts/Interaction/ChangeSceneActivation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSceneActivation : ActivationObject
+{
+    public SceneLoader.Scenes m_toLoad;
+
+
+    public override void activate()
+    {
+        SceneLoader.UnloadAll();    
+        SceneLoader.LoadSceneSync(m_toLoad);
+    }
+
+    public override void deactivate()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool isActivated()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void checkActivationEvent(int index)
+    {
+        throw new NotImplementedException();
+    }
+
+
+}
