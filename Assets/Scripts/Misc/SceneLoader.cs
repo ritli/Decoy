09acdbf55043fbf188a4f4a6828e9d821ba04e7b
William Augustsson <willepojken@gmail.com>
2017-05-17 17:12:22 +0200

    Lasers!
    [CHANGES]
    * LaserPrefab, removed lights
    * SceneLoader, re-added "startFromMenu" Variable.
    * Section3, begun to att lasers!

--- Assets/Scripts/Misc/SceneLoader.cs
+++ Assets/Scripts/Misc/SceneLoader.cs
@@ -22,7 +22,7 @@ public class SceneLoader : MonoBehaviour
     public enum Scenes { InitialScene, InGameBase, MainMenu, Section1a, Section1b, Section2a, Section2b, Section2c, Section3, Section4, Section5, CreditScene, MusicScene, AudioScene };
     public static SceneLoader instance;
     private static List<LoadingScene> m_ScenesLoading;
-
+    public bool startfromMenu;
 	// Use this for initialization
 	void Start ()
     {
