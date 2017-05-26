6364a0102247f9209278f2ebf3d2e9f1c5a3fdb5
ritli <agnar_oskarsson@hotmail.com>
2017-05-26 18:36:11 +0200

    Changes to section 2b & c, section 2 finished

---
+++ Assets/Scripts/EmitterKill.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmitterKill : MonoBehaviour {
+
+	void Start () {
+        KillAll();
+
+        GetComponent<FMODUnity.StudioEventEmitter>().Play();
+
+        print("ALL SOUNDS KILLED");
+    }
+
+    void KillAll()
+    {
+        FMODUnity.StudioEventEmitter[] emitters = FindObjectsOfType<FMODUnity.StudioEventEmitter>();
+
+        foreach(FMODUnity.StudioEventEmitter e in emitters)
+        {
+            e.Stop();
+        }
+    }
+
+
+}
