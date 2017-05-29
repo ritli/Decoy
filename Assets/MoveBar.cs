73ac8a60301da6403383a846f0884b219fb82a08
Eric <eric-noren@hotmail.com>
2017-05-29 11:20:45 +0200

    Added black bars when player is locked

---
+++ Assets/MoveBar.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBar : MonoBehaviour {
+
+    [Tooltip("Determine the amount of pixels moved in y-axis and in what direction.")]
+    public float moveAmount = 100.0f;
+    public float movementSpeed = 20.0f;
+
+
+    public bool isActivated = true;
+    private float originalPosition;
+    private float targetPosition;
+
+    private RectTransform m_transform;
+
+    private void Awake()
+    {
+        m_transform = GetComponent<RectTransform>();
+        originalPosition = m_transform.localPosition.y;
+        targetPosition = m_transform.localPosition.y + moveAmount;
+    }
+
+    public void setActive(bool active)
+    {
+        isActivated = active;
+    }
+
+    public void resetBar()
+    {
+        m_transform.localPosition = new Vector3(m_transform.localPosition.x, originalPosition, m_transform.localPosition.z);
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        setActive(GameManager.GetPlayer().lockPlayer);
+
+        if (!isActivated)
+        {
+            // Move image towards the position.
+            m_transform.localPosition = new Vector3(transform.localPosition.x,
+                Mathf.MoveTowards(transform.localPosition.y, targetPosition, movementSpeed * Time.deltaTime)
+                , transform.localPosition.z);
+
+            if (m_transform.localPosition.y == targetPosition)
+                isActivated = true;
+        }
+        else
+            resetBar();
+	}
+
+}
