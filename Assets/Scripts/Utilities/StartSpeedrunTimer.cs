bd7c27f84c97eeb19baa3519c3a3052cca8dfff0
William Augustsson <willepojken@gmail.com>
2017-05-29 20:47:31 +0200

    Added scripts for speedrunning

---
+++ Assets/Scripts/Utilities/StartSpeedrunTimer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSpeedrunTimer : MonoBehaviour {
+
+    private SpeedrunTimer sprntmr;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        sprntmr = FindObjectOfType<SpeedrunTimer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == Tags.player)
+        {
+            sprntmr.startTimer();
+        }
+    }
+}
